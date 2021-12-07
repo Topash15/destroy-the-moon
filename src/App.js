1aa58ee654e540c45a4719574c34af79bc5ceb8c
Topash15 <Alextopash@gmail.com>
2021-12-07 10:19:07 -0500

    added splashscreen landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Pages/Home'
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./Pages/Home";
+import SplashScreen from "./Pages/Splashscreen/index";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+          {/* <Route path="/splashscreen" element={<SplashScreen/>}/> */}
+          <Route exact path="/" element={<SplashScreen />}/>
+          <Route exact path="/home" element={<Home />}/>
+      </Routes>
     </div>
   );
 }
